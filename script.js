80fdd1594dbaa5a73f4f0be8d1d1696d171b2731
Antonio Guglielmi <antonio.jg@gmail.com>
2021-03-05 20:36:26 +0000

    Development backup

--- script.js
+++ script.js
@@ -1,6 +1,25 @@
 function compute()
 {
-    p = document.getElementById("principal").value;
+
+    // Get the values from the form
+    var principal = document.getElementById("principal").value;
+    var rate = document.getElementById("rate").value;
+    var years = document.getElementById("years").value;
     
+    // Calculate the interest
+    var interest = principal * years * rate / 100;
+
+    // Calculate end year based on current date and input
+    var now = new Date()
+    var end_year = now.getFullYear() + years;
+
+}
+
+function show_rate_value(evt) {
+
+    var rate = evt.value;
+
+    document.getElementById("rangeval").innerHTML(rate);
+
 }
         
